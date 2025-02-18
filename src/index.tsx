f0431663eeabc16a65c48fa295e66abfcf13435e
zhwoddl1001 <zhwoddl0723@gmail.com>
2025-02-18 15:59:22 +0900

    basic commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import PathRoute from "./components/PathRoute";
 
 
 const root = ReactDOM.createRoot(
@@ -8,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-
+  <PathRoute/>
   </React.StrictMode>
 );
 
